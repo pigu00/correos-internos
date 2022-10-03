b077e4583aaba7ba5ef9dfbace5d9f27a1127805
Roberto Marban <robertomarban@gmail.com>
2022-10-03 19:42:15 -0300

    Lista usuarios y registro listo

--- db/model/usuarios.js
+++ db/model/usuarios.js
@@ -6,10 +6,13 @@ class Usuarios extends Model {}
 Usuarios.init(
   {
     idusuario: { type: DataTypes.INTEGER, primaryKey: true,autoIncrement: true },
+    apellido:DataTypes.STRING,
     nombre: DataTypes.STRING,
+    nombreUsuario: DataTypes.STRING,
     contrasena: DataTypes.STRING,
-    ciudad: DataTypes.STRING,
-    pais: DataTypes.STRING
+    recontrasena: DataTypes.STRING,
+    pais: DataTypes.STRING,
+    ciudad: DataTypes.STRING
   },
   {
     sequelize,
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const PORT = 4000;
 const sequelize = require("../db/database")
 // const bodyParse = require ('body-parse') 
 const cors = require('cors')
- 
+const Usuarios = require ('../db/model/usuarios.js')
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,20 +17,31 @@ app.post("/login", (req, res) => {
   });
 
 app.get("/api/users", (req, res) => {
-  res.status(200).json({
-    apellido: "lopez",
-    nombre:"horacio",
-    usuario:"horacio lopez",
-    contrasena:"1234",
-    recontrasena:"1234",
-    pais:"argentina"
+  const listaUsuario = Usuarios.findAll().then(nombreUsuario=>{
+        res.json( nombreUsuario )
+        console.log(listaUsuario)
     })
-  });
+})
+
+
+  
 
 app.post("/api/users", (req, res) => {
-    req.params.apellido
-    console.log(req.params.apellido)
+  Usuarios.create({
+    apellido: req.body.apellido,
+    nombre: req.body.nombre,
+    nombreUsuario: req.body.nombreUsuario,
+    contrasena: req.body.contrasena,
+    recontrasena: req.body.recontrasena,
+    pais: req.body.pais,
+    ciudad: req.body.ciudad
+  }).then((usuario) => {
+    res.json(usuario);
+    console.log(req.body);
   });
+});
+  
+
 
 app.get("/api/:username/messages/inbox", (req, res) => {
     res.send(req.params.username);
@@ -50,11 +61,11 @@ app.get("/api/:username/messages/", (req, res) => {
 
 app.listen(PORT, () => console.log('servidor iniciado en ', PORT))
 
-// sequelize
-//     .sync({ force: true })
-//     .then(() => {
-//       console.log("Conexion exitosa");
-//     })
-//     .catch((error) => {
-//       console.log("Se ha producido un error", error);
-//     });
+sequelize
+    .sync({ force: false })
+    .then(() => {
+      console.log("Conexion exitosa");
+    })
+    .catch((error) => {
+      console.log("Se ha producido un error", error);
+    });
